fa07ac08c433d676c83c6158546ab7d6f86fb2e3
manyyeon <udayeon@naver.com>
2022-09-17 16:59:57 +0900

    navigation setting

---
+++ src/screens/Routes.tsx
@@ -0,0 +1,29 @@
+import {useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './HomeScreen';
+import onBoardingScreen from './OnBoardingScreen';
+
+const RootStack = createNativeStackNavigator();
+
+const RootRoutes = () => {
+  const navigation = useNavigation();
+
+  return (
+    <RootStack.Navigator>
+      <RootStack.Screen name="home" component={HomeScreen} />
+    </RootStack.Navigator>
+  );
+};
+
+const OnBoardingStack = createNativeStackNavigator();
+
+const OnBoardingRoutes = () => {
+  return (
+    <OnBoardingStack.Navigator>
+      <OnBoardingStack.Screen name="onBoarding" component={onBoardingScreen} />
+    </OnBoardingStack.Navigator>
+  );
+};
+
+export {RootRoutes, OnBoardingRoutes};
