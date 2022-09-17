a63bac43464c76767d29de9f0016130a0393456c
유다연 <udayeon@naver.com>
2022-09-17 17:11:33 +0900

    navigation setting (#2)
    * navigation setting
    
    * delete useNavigation

---
+++ src/screens/OnBoardingScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const onBoardingScreen = () => {
+  return (
+    <View>
+      <Text>onBoarding</Text>
+    </View>
+  );
+};
+
+export default onBoardingScreen;
