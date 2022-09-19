c2a1967c288a466a968bbbaf34963c26b1703bd5
manyyeon <udayeon@naver.com>
2022-09-19 16:14:55 +0900

    react-native-kakao-share-link로 카카오톡 링크 공유 기능 구현 실패

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  naechinso_app
+//
+//  Created by 유다연 on 2022/09/19.
+//
+
+import Foundation
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import KakaoShareLink from 'react-native-kakao-share-link';
 
 const HomeScreen = () => {
+  async function testKakaoShare() {
+    try {
+      const response = await KakaoShareLink.sendFeed({
+        content: {
+          title: 'title',
+          imageUrl: '카카오 공유하기 피드에 보여질 이미지URL',
+          link: {
+            mobileWebUrl: 'https://www.naver.com',
+            webUrl: 'https://www.google.com',
+            androidExecutionParams: [{key: 'key1', value: 'value1'}],
+            iosExecutionParams: [
+              {key: 'key1', value: 'value1'},
+              {key: 'key2', value: 'value2'},
+            ],
+          },
+          description: 'description',
+        },
+        buttons: [
+          {
+            title: '앱에서 보기',
+            link: {
+              mobileWebUrl: 'https://www.naver.com',
+              webUrl: 'https://www.google.com',
+              androidExecutionParams: [{key: 'key1', value: 'value1'}],
+              iosExecutionParams: [
+                {key: 'key1', value: 'value1'},
+                {key: 'key2', value: 'value2'},
+              ],
+            },
+          },
+        ],
+      });
+    } catch (e) {
+      console.error(e);
+      // console.error(e.message);
+    }
+  }
+  testKakaoShare();
   return (
     <View>
       <Text>Home</Text>
