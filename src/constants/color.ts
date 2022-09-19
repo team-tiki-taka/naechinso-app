9398f035f3d809a8ada6447ae8b6adc752f386dd
manyyeon <udayeon@naver.com>
2022-09-19 21:03:29 +0900

    color 정리

---
+++ src/constants/color.ts
@@ -0,0 +1,11 @@
+const color = {
+  orange: '#FF7A00',
+  orange20: '#FFE4CC',
+  black: '#000000',
+  black20: '#CCCCCC',
+  black40: '#999999',
+  white: '#FFFFFF',
+  neural: '#F6F5F2',
+};
+
+export default color;
