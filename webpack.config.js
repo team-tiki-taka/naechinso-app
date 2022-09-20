c37ebe05653e416e1334fb5fc8a39372d509c090
boxfox619 <boxfoxsg619@gmail.com>
2022-09-20 15:26:00 +0900

    fix image loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,16 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+          // publicPath: 'https://cdn.example.com/', // uncomment to override webpack public path
+          // esModule: true
+          scalings: {'@2x': 2, '@3x': 3},
+        },
+        loader: 'react-native-web-image-loader',
+      },
     ],
   },
   plugins: [HTMLWebpackPluginConfig],
