4d08fa54c7f0007e8cf9ff1a78edf0935adc0038
유다연 <udayeon@naver.com>
2022-09-20 14:44:33 +0900

    Feat/sharing kakao link with share (#5)
    * navigation setting
    
    * delete useNavigation
    
    * 링크 공유하기 기능 추가
    
    * vscode 설정 변경
    
    Co-authored-by: 김성래 - Boxfox <boxfoxsg619@gmail.com>

--- src/screens/Routes.tsx
+++ src/screens/Routes.tsx
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import HomeScreen from './HomeScreen';
 import onBoardingScreen from './OnBoardingScreen';
+import SharingLink from './SharingLink';
 
 const RootStack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const RootRoutes = () => {
   return (
     <RootStack.Navigator>
       <RootStack.Screen name="home" component={HomeScreen} />
+      <RootStack.Screen name="sharingLink" component={SharingLink} />
     </RootStack.Navigator>
   );
 };
