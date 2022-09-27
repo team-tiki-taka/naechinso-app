06cac84e4a26f546e47f7d73118060673eaaa649
manyyeon <udayeon@naver.com>
2022-09-27 15:46:16 +0900

    WIP : 시작화면 추가

---
+++ src/screens/StartScreen.tsx
@@ -0,0 +1,33 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import styled from 'styled-components/native';
+import Button from '../components/Button';
+import {Flex} from '../components/Flex';
+import Screen from '../components/Screen';
+import {Text, Typography} from '../components/text';
+
+const StartScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <Screen>
+      <Flex align="center">
+        <Button
+          onPress={() => {
+            navigation.navigate('signIn');
+          }}
+          type="primary"
+          width={335}
+          height={56}>
+          내친소 시작하기
+        </Button>
+        <Text typography={Typography.Caption_2_M}>
+          😎 내 친구를 소개하고 싶어
+        </Text>
+      </Flex>
+    </Screen>
+  );
+};
+
+const StartContainer = styled.View``;
+
+export default StartScreen;
