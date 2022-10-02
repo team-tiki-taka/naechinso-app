33145049934d6756c3e76ae7204709db8b5367cb
manyyeon <udayeon@naver.com>
2022-10-02 22:56:52 +0900

    verifySMSCode response type 정의

--- src/remotes/auth/verifySMSCode.ts
+++ src/remotes/auth/verifySMSCode.ts
@@ -1,6 +1,19 @@
 import {getRequester} from '@remotes/requester';
 
-export async function verifySMSCode(phoneNumber: string, code: string) {
+interface newMemberData {
+  registerToken: string;
+  recommendReceived: boolean;
+}
+
+interface existingMemberData {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export async function verifySMSCode(
+  phoneNumber: string,
+  code: string,
+): Promise<newMemberData | existingMemberData> {
   const res = await getRequester().post('/sms/verify', {phoneNumber, code});
   return res.data;
 }
