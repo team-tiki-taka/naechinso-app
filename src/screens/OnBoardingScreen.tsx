efda32a422f69020394495c3ab828a6a32ed7d91
boxfox619 <boxfoxsg619@gmail.com>
2022-10-02 14:46:29 +0900

    fix navigation

--- src/screens/OnBoardingScreen.tsx
+++ src/screens/OnBoardingScreen.tsx
@@ -1,10 +1,10 @@
 import Button from '@components/Button';
+import {useOnboardingNavigation} from '@hooks/navigation';
 import React from 'react';
 import {Text, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
 
-const onBoardingScreen = () => {
-  const navigation = useNavigation();
+export default function OnboardingScreen() {
+  const navigation = useOnboardingNavigation();
   return (
     <View>
       <Text>onBoarding</Text>
@@ -16,6 +16,4 @@ const onBoardingScreen = () => {
       </Button>
     </View>
   );
-};
-
-export default onBoardingScreen;
+}
