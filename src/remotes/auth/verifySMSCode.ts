9c91d861d155f77d6b215ed3741f113b46913fff
manyyeon <udayeon@naver.com>
2022-10-03 19:03:49 +0900

    verifySMSCode api responseType 변경

--- src/remotes/auth/verifySMSCode.ts
+++ src/remotes/auth/verifySMSCode.ts
@@ -1,19 +1,26 @@
 import {getRequester} from '@remotes/requester';
 
-interface NewMemberData {
+export interface NewMemberData {
   registerToken: string;
   recommendReceived: boolean;
 }
 
-interface ExistingMemberData {
+export interface ExistingMemberData {
   accessToken: string;
   refreshToken: string;
+}
+
+export interface ResponseType {
+  timestamp: string;
+  status: number;
+  success: boolean;
+  data: NewMemberData | ExistingMemberData;
 }
 
 export async function verifySMSCode(
   phoneNumber: string,
   code: string,
-): Promise<NewMemberData | ExistingMemberData> {
+): Promise<ResponseType> {
   const res = await getRequester().post('/sms/verify', {phoneNumber, code});
   return res.data;
 }
