d22e894525648c1af7bc812d185fd9ef6657fe8b
manyyeon <udayeon@naver.com>
2022-10-06 02:05:54 +0900

    Member 정보가 맞지 않는 경우 화면 추가

---
+++ src/screens/memberJoin/RecommendReceived/MemberInfoIncorrectScreen.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {useOnboardingNavigation} from '@hooks/navigation';
+import {PageHeader} from '@components/PageHeader';
+import {Flex, Screen} from '@components/layout';
+import {UserBaseInfoForm} from '@components/UserBaseInfoForm';
+import {useForm} from 'react-hook-form';
+import {UserBaseInfo} from '@models/UserBaseInfo';
+import styled from 'styled-components/native';
+import {BottomCTAButton} from '@components/button';
+import {AppBar} from '@components/common';
+
+export const MemberInfoIncorrectScreen = () => {
+  const navigation = useOnboardingNavigation();
+
+  const controls = useForm<UserBaseInfo>({
+    mode: 'all',
+  });
+
+  return (
+    <Screen>
+      <AppBar />
+      <PageHeader title={'웁스 😅\n친구가 급한 마음에 실수했나봐~'} />
+      <Flex justify="space-between" style={{flex: 1}}>
+        <InnerContainer>
+          <UserBaseInfoForm controls={controls} />
+        </InnerContainer>
+      </Flex>
+      <BottomCTAButton
+        onPress={() => {
+          navigation.navigate('InputHeight');
+        }}>
+        완료
+      </BottomCTAButton>
+    </Screen>
+  );
+};
+
+const InnerContainer = styled.View`
+  padding-horizontal: 24px;
+  padding-top: 24px;
+`;
