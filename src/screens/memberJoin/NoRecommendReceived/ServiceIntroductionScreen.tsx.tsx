cdd36ff5dd0a444d46ec201b81215cbc4a869e91
manyyeon <udayeon@naver.com>
2022-10-05 16:13:57 +0900

    ServiceIntroductionScreen 컴포넌트 사용 안하는 구조로 변경

--- src/screens/memberJoin/NoRecommendReceived/ServiceIntroductionScreen.tsx.tsx
+++ src/screens/memberJoin/NoRecommendReceived/ServiceIntroductionScreen.tsx.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import {useOnboardingNavigation} from '@hooks/navigation';
-import {ServiceIntroduction} from '@screens/onboarding/components/ServiceIntroduction';
+import {Text, Typography} from '@components/text';
+import {Flex, Screen} from '@components/layout';
+import {Spacing} from '@components/common';
+import colors from '@constants/color';
+import {BottomCTAButton} from '@components/button';
+import styled from 'styled-components/native';
 
 export const ServiceIntroductionNoRecommendScreen = () => {
   const navigation = useOnboardingNavigation();
 
   return (
-    <ServiceIntroduction
-      titles={[
-        '안녕 😎',
-        '',
-        '내친소를 시작하려면',
-        '친구에게 추천사를 받아야 해',
-        '',
-        '너를 잘 아는 사람에게',
-        '부탁해봐 🙏🏻',
-      ]}
-      buttonText="추천사 부탁하기"
-      onPress={() => {
-        navigation.navigate('InputMemberInfo');
-      }}
-    />
+    <Screen backgroundColor={colors.white}>
+      <Spacing height={52} />
+      <Flex justify="space-between" style={{flex: 1}}>
+        <InnerContainer>
+          <Text typography={Typography.Headline_1_B}>안녕 😎 </Text>
+          <Spacing height={20} />
+          <Text typography={Typography.Headline_1_B}>
+            {'내친소를 시작하려면\n친구에게 추천사를 받아야 해'}
+          </Text>
+        </InnerContainer>
+        <BottomCTAButton
+          onPress={() => {
+            navigation.navigate('InputMemberInfo');
+          }}>
+          추천사 부탁하기
+        </BottomCTAButton>
+      </Flex>
+    </Screen>
   );
 };
+
+const InnerContainer = styled.View`
+  padding-horizontal: 24px;
+`;
