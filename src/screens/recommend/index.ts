717c95439b6e8a1c4f05fbd6175575fee90d6880
manyyeon <udayeon@naver.com>
2022-10-07 13:39:55 +0900

    추천사 작성 screens index.ts 추가

---
+++ src/screens/recommend/index.ts
@@ -0,0 +1,5 @@
+export * from './InputFriendBaseInfoScreen';
+export * from './RecommendServiceIntroductionScreen';
+export * from './InputFriendMeetScreen';
+export * from './InputFriendMeetTermScreen';
+export * from './InputFriendPersonalityScreen';
