db37580809c3b70736062d3d7393656b4c80c324
manyyeon <udayeon@naver.com>
2022-10-07 13:37:57 +0900

    소개할 친구 만난 계기 화면 추가

---
+++ src/screens/recommend/InputFriendMeetScreen.tsx
@@ -0,0 +1,120 @@
+import React, {useState} from 'react';
+import {AppBar, Spacing} from '@components/common';
+import {AutoScrollView, Flex, InnerContainer, Screen} from '@components/layout';
+import {PageHeader} from '@components/PageHeader';
+import {BottomCTAButton, ToggleButton} from '@components/button';
+import {useOnboardingNavigation} from '@hooks/navigation';
+import {useBooleanState} from '@hooks/common';
+import {TextInput} from 'react-native';
+import colors from '@constants/color';
+import {Text, Typography} from '@components/text';
+import {Controller, useForm} from 'react-hook-form';
+
+type MeetType = 'family' | 'school' | 'university' | 'business';
+
+export const InputFriendMeetScreen = () => {
+  const navigation = useOnboardingNavigation();
+  const [selected, setSelected] = useState<MeetType | string>('');
+  const [isEtc, setIsEtcTrue, setIsEtcFalse] = useBooleanState(false);
+  const controls = useForm({
+    mode: 'all',
+  });
+  const {control} = controls;
+
+  return (
+    <Screen>
+      <AppBar />
+      <PageHeader title={'어떻게 만난 사이야?'} />
+      <Spacing height={24} />
+      <Flex justify="space-between" style={{flex: 1}}>
+        <Controller
+          control={control}
+          name="meet"
+          render={({field: {value, onChange}}) => (
+            <AutoScrollView>
+              <InnerContainer>
+                <ToggleButton
+                  type="brownMain"
+                  active={value === 'family'}
+                  onPress={() => {
+                    onChange('family');
+                    setIsEtcFalse();
+                  }}>
+                  친족
+                </ToggleButton>
+                <Spacing height={16} />
+                <ToggleButton
+                  type="brownMain"
+                  active={value === 'school'}
+                  onPress={() => {
+                    onChange('school');
+                    setIsEtcFalse();
+                  }}>
+                  초/중/고 친구
+                </ToggleButton>
+                <Spacing height={16} />
+                <ToggleButton
+                  type="brownMain"
+                  active={value === 'university'}
+                  onPress={() => {
+                    onChange('university');
+                    setIsEtcFalse();
+                  }}>
+                  대학교 친구
+                </ToggleButton>
+                <Spacing height={16} />
+                <ToggleButton
+                  type="brownMain"
+                  active={value === 'business'}
+                  onPress={() => {
+                    onChange('business');
+                    setIsEtcFalse();
+                  }}>
+                  회사 친구
+                </ToggleButton>
+                <Spacing height={16} />
+                <ToggleButton
+                  type="brownMain"
+                  active={isEtc}
+                  onPress={() => {
+                    setIsEtcTrue();
+                    onChange('');
+                  }}>
+                  기타
+                </ToggleButton>
+                <Spacing height={4} />
+                {isEtc && (
+                  <Flex direction="row">
+                    <Text
+                      typography={Typography.Subtitle_2_M}
+                      color={colors.brown}>
+                      {': '}
+                    </Text>
+                    <TextInput
+                      value={value}
+                      onChangeText={onChange}
+                      placeholder={'그러면 어떻게 만났어?'}
+                      placeholderTextColor={colors.black20}
+                    />
+                  </Flex>
+                )}
+              </InnerContainer>
+            </AutoScrollView>
+          )}
+        />
+
+        <Spacing height={41} />
+        <BottomCTAButton
+          onPress={() => {
+            if (isEtc) {
+              navigation.navigate('InputFriendMeetTerm');
+            } else {
+              navigation.navigate('InputFriendPersonality');
+            }
+          }}>
+          다음
+        </BottomCTAButton>
+      </Flex>
+    </Screen>
+  );
+};
