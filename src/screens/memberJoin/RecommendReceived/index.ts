68f786f9638af990c57a6096c99c9a3112d75812
manyyeon <udayeon@naver.com>
2022-10-07 13:36:54 +0900

    추천사가 있는 경우 screens index.ts 추가

---
+++ src/screens/memberJoin/RecommendReceived/index.ts
@@ -0,0 +1,4 @@
+export * from './CheckMemberBaseInfoScreen';
+export * from './InputHeightScreen';
+export * from './MemberBaseInfoIncorrectScreen';
+export * from './MemberServiceIntroductionRecommendScreen';
