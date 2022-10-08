c4141d4925b5993d9fc9fb4ebb31d282f447ba0c
manyyeon <udayeon@naver.com>
2022-10-08 21:16:23 +0900

    add 추천인 학생인증 screen

---
+++ src/screens/onboarding/recommend/InputRecommenderStudentScreen.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import {AppBar, Spacing} from '@components/common';
+import {Flex, InnerContainer, Screen} from '@components/layout';
+import {PageHeader} from '@components/PageHeader';
+import {TextField} from '@components/form';
+import {BottomCTAButton} from '@components/button';
+import {useOnboardingNavigation} from '@hooks/navigation';
+import {useBooleanState} from '@hooks/common';
+
+export const InputRecommenderStudentScreen = () => {
+  const navigation = useOnboardingNavigation();
+  const [school, setSchool] = useState<string>('');
+  const [schoolType, setSchoolType] = useState<
+    '대학교' | '고등학교' | '중학교'
+  >('대학교');
+  const [major, setMajor] = useState<string>('');
+  const [buttonIsActive, setButtonIsActiveTrue, setButtonIsActiveFalse] =
+    useBooleanState();
+
+  useEffect(() => {
+    if (schoolType === '대학교') {
+      school && major ? setButtonIsActiveTrue() : setButtonIsActiveFalse();
+    } else {
+      school ? setButtonIsActiveTrue() : setButtonIsActiveFalse();
+    }
+  }, [school, major]);
+
+  return (
+    <Screen>
+      <AppBar />
+      <PageHeader title={'🏫\n최종 학력을 적어줘!'} />
+      <Spacing height={24} />
+      <Flex justify="space-between" style={{flex: 1}}>
+        <InnerContainer>
+          <Flex direction="row" justify="space-between">
+            <TextField
+              containerStyle={{minWidth: '45%'}}
+              label={'학교명'}
+              value={school}
+              onChangeText={setSchool}
+            />
+            <Spacing width={12} />
+            <TextField label={'학교'} value={schoolType} />
+          </Flex>
+          {schoolType === '대학교' && (
+            <>
+              <Spacing height={16} />
+              <TextField label={'전공'} value={major} onChangeText={setMajor} />
+            </>
+          )}
+        </InnerContainer>
+        <BottomCTAButton
+          disabled={!buttonIsActive}
+          onPress={() => {
+            navigation.navigate('VerifyStudent');
+          }}>
+          다음
+        </BottomCTAButton>
+      </Flex>
+    </Screen>
+  );
+};
