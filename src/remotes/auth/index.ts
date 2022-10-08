6217e28bd5cea3993a60f627ec028483ef73883f
boxfox619 <boxfoxsg619@gmail.com>
2022-10-08 20:02:08 +0900

    fix export

--- src/remotes/auth/index.ts
+++ src/remotes/auth/index.ts
@@ -1,4 +1,4 @@
-export * from './sendSMSCode';
+export * from './sendSmsCode';
 export * from './verifySMSCode';
 export * from './fetchCurrentUser';
 export * from './submitSignupInfo';
--- src/remotes/recommend/requestRecommend.ts
+++ src/remotes/recommend/requestRecommend.ts
@@ -1,6 +1,7 @@
 import {ServerResponse} from '@models/ServerResponse';
 import {mainRequester} from '@remotes/requester';
 
+// 다른 유저에게 추천서 작성을 요청한다 (AccessToken)
 export async function requestRecommend(data: RequestRecommendPayload) {
   const res = await mainRequester.post<ServerResponse>(
     '/recommend/request',
