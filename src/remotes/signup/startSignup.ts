352959d7089e870fd77d3ffba9eace8d1db2b963
김성래 - Boxfox <boxfoxsg619@gmail.com>
2022-10-08 20:29:33 +0900

    add access token logic (#38)

--- src/remotes/signup/startSignup.ts
+++ src/remotes/signup/startSignup.ts
@@ -1,9 +1,16 @@
 import {Gender} from '@models/Gender';
 import {ServerResponse} from '@models/ServerResponse';
+import {setAccessToken} from '@remotes/access-token';
 import {mainRequester} from '@remotes/requester';
 
 export async function startSignup(data: StartSignupPayload) {
-  const res = await mainRequester.post<ServerResponse>('/member/join', data);
+  const res = await mainRequester.post<ServerResponse<{accessToken: string}>>(
+    '/member/join',
+    data,
+  );
+  if (res.data.success && res.data.data.accessToken) {
+    await setAccessToken(res.data.data.accessToken);
+  }
   return res.data.success;
 }
 
