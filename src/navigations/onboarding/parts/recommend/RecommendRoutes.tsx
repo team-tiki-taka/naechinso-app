d6e2f04dc096549eae3345e65863f1df2bc973e4
manyyeon <udayeon@naver.com>
2022-10-11 01:58:42 +0900

    추가한 screen들 routes에 추가

--- src/navigations/onboarding/parts/recommend/RecommendRoutes.tsx
+++ src/navigations/onboarding/parts/recommend/RecommendRoutes.tsx
@@ -1,5 +1,21 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {ServiceIntroductionNoRecommendScreen} from '@screens/memberJoin/NoRecommendReceived/ServiceIntroductionScreen.tsx';
+import {
+  InputFriendBaseInfoScreen,
+  InputFriendMeetScreen,
+  InputFriendMeetTermScreen,
+  InputFriendPersonalityMoreScreen,
+  InputFriendPersonalityScreen,
+  InputFriendPhoneNumScreen,
+  InputRecommenderBaseInfoScreen,
+  InputRecommenderCompanyScreen,
+  InputRecommenderStudentScreen,
+  RecommenderSelfIntroductionStartScreen,
+  RecommendServiceIntroductionScreen,
+  RecommendShareLinkScreen,
+  VerifyRecommenderScreen,
+  VerifyRecommenderStudentScreen,
+} from '@screens/onboarding/recommend';
+import {VerifyRecommenderCompanyScreen} from '@screens/onboarding/recommend/VerifyRecommenderCompanyScreen';
 import React from 'react';
 import {RecommendStackParamList} from './RecommendRouteTypes';
 
@@ -9,10 +25,66 @@ export const RecommendRoutes = () => {
   return (
     <RecommendStack.Navigator
       screenOptions={{headerShown: false}}
-      initialRouteName="ServiceIntroductionNoRecommend">
+      initialRouteName="RecommendServiceIntroduction">
       <RecommendStack.Screen
-        name="ServiceIntroductionNoRecommend"
-        component={ServiceIntroductionNoRecommendScreen}
+        name="RecommendServiceIntroduction"
+        component={RecommendServiceIntroductionScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendBaseInfo"
+        component={InputFriendBaseInfoScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendMeet"
+        component={InputFriendMeetScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendMeetTerm"
+        component={InputFriendMeetTermScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendPersonality"
+        component={InputFriendPersonalityScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendPersonalityMore"
+        component={InputFriendPersonalityMoreScreen}
+      />
+      <RecommendStack.Screen
+        name="InputFriendPhoneNum"
+        component={InputFriendPhoneNumScreen}
+      />
+      <RecommendStack.Screen
+        name="RecommenderSelfIntroductionStart"
+        component={RecommenderSelfIntroductionStartScreen}
+      />
+      <RecommendStack.Screen
+        name="InputRecommenderBaseInfo"
+        component={InputRecommenderBaseInfoScreen}
+      />
+      <RecommendStack.Screen
+        name="VerifyRecommender"
+        component={VerifyRecommenderScreen}
+      />
+      <RecommendStack.Screen
+        name="InputRecommenderCompany"
+        component={InputRecommenderCompanyScreen}
+      />
+      <RecommendStack.Screen
+        name="VerifyRecommenderCompany"
+        component={VerifyRecommenderCompanyScreen}
+      />
+      <RecommendStack.Screen
+        name="InputRecommenderStudent"
+        component={InputRecommenderStudentScreen}
+      />
+      <RecommendStack.Screen
+        name="VerifyRecommenderStudent"
+        component={VerifyRecommenderStudentScreen}
+      />
+      <RecommendStack.Screen
+        name="RecommendShareLink"
+        component={RecommendShareLinkScreen}
       />
     </RecommendStack.Navigator>
   );
