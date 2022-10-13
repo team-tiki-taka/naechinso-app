acb093d479cd65b14f1c44eaaf57ab70f75eea21
manyyeon <udayeon@naver.com>
2022-10-13 10:20:47 +0900

    추천인 학교 입력 screen wheelPicker 추가

--- src/screens/onboarding/recommend/InputRecommenderStudentScreen.tsx
+++ src/screens/onboarding/recommend/InputRecommenderStudentScreen.tsx
@@ -6,24 +6,63 @@ import {TextField} from '@components/form';
 import {BottomCTAButton} from '@components/button';
 import {useOnboardingNavigation} from '@hooks/navigation';
 import {useBooleanState} from '@hooks/common';
+import styled from 'styled-components/native';
+import colors from '@constants/color';
+import {Text, Typography} from '@components/text';
+import {useWheelPickerSheet} from '@hooks/form';
 
 export const InputRecommenderStudentScreen = () => {
+  const SCHOOL_TYPE = {
+    univ: {
+      typeName: '대학교',
+    },
+    high: {
+      typeName: '고등학교',
+    },
+    mid: {
+      typeName: '중학교',
+    },
+  };
+
   const navigation = useOnboardingNavigation();
-  const [school, setSchool] = useState<string>('');
-  const [schoolType, setSchoolType] = useState<
-    '대학교' | '고등학교' | '중학교'
-  >('대학교');
-  const [major, setMajor] = useState<string>('');
+  const [data, setData] = useState<{
+    school: string;
+    schoolType: 'univ' | 'high' | 'mid';
+    major: string;
+  }>({
+    school: '',
+    schoolType: 'univ',
+    major: '',
+  });
+
   const [buttonIsActive, setButtonIsActiveTrue, setButtonIsActiveFalse] =
     useBooleanState();
 
   useEffect(() => {
-    if (schoolType === '대학교') {
-      school && major ? setButtonIsActiveTrue() : setButtonIsActiveFalse();
+    if (data.schoolType === 'univ') {
+      data.school && data.major
+        ? setButtonIsActiveTrue()
+        : setButtonIsActiveFalse();
     } else {
-      school ? setButtonIsActiveTrue() : setButtonIsActiveFalse();
+      data.school ? setButtonIsActiveTrue() : setButtonIsActiveFalse();
     }
-  }, [school, major]);
+  }, [data.school, data.major]);
+
+  const open = useWheelPickerSheet(
+    '타이틀', //타이틀
+    [
+      // 선택지
+      {label: SCHOOL_TYPE.univ.typeName, value: 'univ'},
+      {label: SCHOOL_TYPE.high.typeName, value: 'high'},
+      {label: SCHOOL_TYPE.mid.typeName, value: 'mid'},
+    ],
+    'univ', // 기본값
+  );
+
+  const setSchoolType = async () => {
+    const value = await open();
+    setData({...data, schoolType: value});
+  };
 
   return (
     <Screen>
@@ -34,18 +73,43 @@ export const InputRecommenderStudentScreen = () => {
         <StyledInnerContainer>
           <Flex direction="row" justify="space-between">
             <TextField
-              containerStyle={{minWidth: '45%'}}
+              containerStyle={{minWidth: '50%'}}
               label={'학교명'}
-              value={school}
-              onChangeText={setSchool}
+              value={data.school}
+              onChangeText={text => {
+                setData({...data, school: text});
+              }}
             />
             <Spacing width={12} />
-            <TextField label={'학교'} value={schoolType} />
+            <StyledAgeContainer
+              onPress={() => {
+                setSchoolType();
+              }}>
+              <Text typography={Typography.Body_1_M} color={colors.black40}>
+                학교
+              </Text>
+              <Flex.CenterVertical direction="row">
+                <Text typography={Typography.Subtitle_1_B} color={colors.black}>
+                  {SCHOOL_TYPE[data.schoolType].typeName}
+                </Text>
+                <Spacing width={24} />
+                <Icon
+                  style={{transform: [{rotate: '180deg'}]}}
+                  source={require('@assets/icons/ic_chevron_down_black.png')}
+                />
+              </Flex.CenterVertical>
+            </StyledAgeContainer>
           </Flex>
-          {schoolType === '대학교' && (
+          {data.schoolType === 'univ' && (
             <>
               <Spacing height={16} />
-              <TextField label={'전공'} value={major} onChangeText={setMajor} />
+              <TextField
+                label={'전공'}
+                value={data.major}
+                onChangeText={text => {
+                  setData({...data, major: text});
+                }}
+              />
             </>
           )}
         </StyledInnerContainer>
@@ -60,3 +124,20 @@ export const InputRecommenderStudentScreen = () => {
     </Screen>
   );
 };
+
+const StyledAgeContainer = styled.TouchableOpacity`
+  display: flex;
+  height: 80px;
+  background-color: ${colors.neural};
+  border-radius: 16px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
+  border-width: 1px;
+  border-color: ${colors.neural};
+`;
+
+const Icon = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
