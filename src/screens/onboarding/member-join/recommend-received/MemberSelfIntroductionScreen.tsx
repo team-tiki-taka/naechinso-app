71c0a5570f77b5ba750f9d85378e3c01a6afaa10
manyyeon <udayeon@naver.com>
2022-10-13 22:15:01 +0900

    add MemberSelfIntroductionScreen

---
+++ src/screens/onboarding/member-join/recommend-received/MemberSelfIntroductionScreen.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {Flex, Screen, StyledInnerContainer} from '@components/layout';
+import {AppBar, Spacing} from '@components/common';
+import {PageHeader} from '@components/PageHeader';
+import {TextArea} from '@components/form';
+import {BottomCTAButton} from '@components/button';
+import {useOnboardingNavigation} from '@hooks/navigation';
+
+export function MemberSelfIntroductionScreen() {
+  const navigation = useOnboardingNavigation();
+  const [personalityMore, setPersonalityMore] = useState<string>();
+  const handleCTAPress = () => {
+    navigation.navigate('InputMemberRomanticStyle');
+  };
+  const isDisabled = Boolean(!personalityMore);
+  return (
+    <Screen>
+      <AppBar />
+      <PageHeader title={'남들보다 내가 이건 좀 낫지!\n하는 게 있을까?😎 '} />
+      <Spacing height={24} />
+      <Flex justify="space-between" style={{flex: 1}}>
+        <StyledInnerContainer>
+          <TextArea
+            value={personalityMore}
+            onChangeText={setPersonalityMore}
+            placeholder={
+              '- 난 고민을 잘 들어줘\n- 옷을 잘 입는 편이야\n- 맛집을 잘 알아'
+            }
+          />
+        </StyledInnerContainer>
+        <BottomCTAButton disabled={isDisabled} onPress={handleCTAPress}>
+          다음
+        </BottomCTAButton>
+      </Flex>
+    </Screen>
+  );
+}
