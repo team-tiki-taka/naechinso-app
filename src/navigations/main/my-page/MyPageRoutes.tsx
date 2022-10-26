1151760adc5b7ed49db2848948d516a751280b37
manyyeon <udayeon@naver.com>
2022-10-26 13:56:26 +0900

    MyPageRoutes 추가

--- src/navigations/main/my-page/MyPageRoutes.tsx
+++ src/navigations/main/my-page/MyPageRoutes.tsx
@@ -1,4 +1,9 @@
-import {MyProfileScreen} from '@screens/my-page';
+import {
+  ModifyMyProfileScreen,
+  MyProfileScreen,
+  ReceiveHeartScreen,
+  SendHeartScreen,
+} from '@screens/my-page';
 import React from 'react';
 import {MainStackParamList} from '../MainRouteTypes';
 import {NativeStackNavigator} from '../NativeStackNavigator';
@@ -6,5 +11,10 @@ import {NativeStackNavigator} from '../NativeStackNavigator';
 export const createMyPageRoutes = (
   Stack: NativeStackNavigator<MainStackParamList>,
 ) => {
-  return [<Stack.Screen name="MyProfile" component={MyProfileScreen} />];
+  return [
+    <Stack.Screen name="MyProfile" component={MyProfileScreen} />,
+    <Stack.Screen name="ModifyMyProfile" component={ModifyMyProfileScreen} />,
+    <Stack.Screen name="SendHeart" component={SendHeartScreen} />,
+    <Stack.Screen name="ReceiveHeart" component={ReceiveHeartScreen} />,
+  ];
 };
