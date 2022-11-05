074a15a71983b221f6828112dd707a4b5af8d01a
manyyeon <udayeon@naver.com>
2022-11-06 06:10:33 +0900

    data 없을 때 이미지 추가

--- src/screens/my-page/MyPageHomeScreen.tsx
+++ src/screens/my-page/MyPageHomeScreen.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import {Spacing} from '@components/common';
 import {
+  Flex,
   Screen,
   StyledInnerContainer,
   TransparentGradient,
 } from '@components/layout';
 import colors from '@constants/color';
-import {ProfileCard} from './components/ProfileCard';
-import {Gender} from '@models/Gender';
-import {Spacing} from '@components/common';
-import {SectionList} from 'react-native';
-import {MyPageHeader} from '@screens/my-page/components/my-page-header/MyPageHeader';
-import styled from 'styled-components/native';
 import {useMainNavigation} from '@hooks/navigation';
+import {Gender} from '@models/Gender';
+import {MyPageHeader} from '@screens/my-page/components/my-page-header/MyPageHeader';
+import React from 'react';
+import {SectionList} from 'react-native';
+import {useRecoilValue} from 'recoil';
+import styled from 'styled-components/native';
 import {ToggleMenu} from './components/my-page-header';
 import {ProfileHeader} from './components/my-page-header/ProfileHeader';
+import {ProfileCard} from './components/ProfileCard';
 import {useToggleMenu} from './hooks';
+
+import {Text, Typography} from '@components/text';
+
+import img_no_complete_heart from '@assets/images/img_no_complete_heart.png';
+import img_no_give_heart from '@assets/images/img_no_give_heart.png';
+import img_no_take_heart from '@assets/images/img_no_take_heart.png';
+import React, {useEffect, useState} from 'react';
 
 export function MyPageHomeScreen() {
   const navigation = useMainNavigation();
+  const liked = useRecoilValue(likedMatchesState);
+  console.log(liked);
+  //const completed = useRecoilValue(completedMatchesState);
+
   const handlePress = () => {
     navigation.navigate('MyProfile');
   };
@@ -34,6 +47,30 @@ export function MyPageHomeScreen() {
       navigation.navigate('Profile');
     }
   };
+
+  // fetchReceivedMatches()
+  //   .then(res => {
+  //     console.log(res);
+  //   })
+  //   .catch(e => {
+  //     console.log(e.response);
+  //   });
+
+  // fetchCompleteMatches()
+  //   .then(res => {
+  //     console.log(res);
+  //   })
+  //   .catch(e => {
+  //     console.log(e);
+  //   });
+
+  // fetchSendedMatches()
+  //   .then(res => {
+  //     console.log(res);
+  //   })
+  //   .catch(e => {
+  //     console.log(e);
+  //   });
 
   const data = [
     {
@@ -54,14 +91,49 @@ export function MyPageHomeScreen() {
     },
   ];
 
+  const [noDataInfo, setNoDataInfo] = useState<{
+    image: string;
+    title: string;
+    content: string;
+  }>({});
+
+  useEffect(() => {
+    if (selectedMenu.menu === '보낸 호감') {
+      setNoDataInfo({
+        image: img_no_give_heart,
+        title: '보낸 호감이 없어',
+        content: '마음에 쏙 드는 사람을 \n찾을 수 있도록 더 노력할게!',
+      });
+    } else if (selectedMenu.menu === '받은 호감') {
+      setNoDataInfo({
+        image: img_no_take_heart,
+        title: '받은 호감이 없어',
+        content: '조금만 기다려봐!',
+      });
+    } else {
+      setNoDataInfo({
+        image: img_no_complete_heart,
+        title: '둘 다 호감이 없어',
+        content: '서로 마음에 쏙 드는 사람이\n곧 나타날거야 :)',
+      });
+    }
+  }, [selectedMenu]);
+
   return (
     <Screen>
       <MyPageHeader />
       <SectionList
         sections={data}
-        keyExtractor={(item, index) => {
-          return index;
-        }}
+        renderSectionFooter={item =>
+          item.section.title === 'CardList' &&
+          !item.section.data.length && (
+            <NoData
+              image={noDataInfo?.image}
+              title={noDataInfo?.title}
+              content={noDataInfo?.content}
+            />
+          )
+        }
         renderItem={item => {
           if (item.section.title === 'CardList') {
             return (
@@ -95,3 +167,33 @@ export function MyPageHomeScreen() {
 const InnerContainer = styled(StyledInnerContainer)`
   background-color: ${colors.neural};
 `;
+
+const StyledImage = styled.Image`
+  width: 154px;
+  height: 124px;
+`;
+
+function NoData({
+  image,
+  title,
+  content,
+}: {
+  image: string;
+  title: string;
+  content: string;
+}) {
+  return (
+    <Flex.CenterVertical>
+      <Spacing height={116} />
+      <StyledImage source={image} />
+      <Spacing height={11} />
+      <Text typography={Typography.Subtitle_1_B} color={colors.black20}>
+        {title}
+      </Text>
+      <Spacing height={6} />
+      <Text typography={Typography.Body_2_M} color={colors.black40} center>
+        {content}
+      </Text>
+    </Flex.CenterVertical>
+  );
+}
