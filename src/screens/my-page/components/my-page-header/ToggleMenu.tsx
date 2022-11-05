7cd28153f12619e999fa4f83097442afa11b9e08
manyyeon <udayeon@naver.com>
2022-11-06 01:52:05 +0900

    ToggleMenu UI 추가

--- src/screens/my-page/components/my-page-header/ToggleMenu.tsx
+++ src/screens/my-page/components/my-page-header/ToggleMenu.tsx
@@ -6,6 +6,9 @@ import {MenuListType, MenuType} from '@screens/my-page/hooks/useToggleMenu';
 import React, {ReactNode} from 'react';
 import styled from 'styled-components/native';
 
+import ic_heart_black20 from '@assets/icons/ic_heart_black20.png';
+import ic_heart_orange from '@assets/icons/ic_heart_orange.png';
+
 export function ToggleMenu({
   selectedMenu,
   handleSelect,
@@ -21,9 +24,13 @@ export function ToggleMenu({
         return (
           <React.Fragment key={idx}>
             <StyledMenu onPress={handleSelect(idx)}>
-              <StyledText active={field === selectedMenu.menu}>
-                {field}
-              </StyledText>
+              <Flex direction="row">
+                <StyledText
+                  active={field === selectedMenu.menu}
+                  icon={idx === 2}>
+                  {field}
+                </StyledText>
+              </Flex>
             </StyledMenu>
             {idx !== fields.length - 1 && <Spacing width={36} />}
           </React.Fragment>
@@ -38,19 +45,36 @@ const StyledMenu = styled.TouchableOpacity``;
 function StyledText({
   active,
   children,
+  icon = false,
 }: {
   active: boolean;
   children: ReactNode;
+  icon: boolean;
 }) {
   return (
-    <>
-      <Text
-        typography={active ? Typography.Body_1_B : Typography.Body_1_M}
-        color={active ? colors.orange : colors.black40}>
-        {children}
-      </Text>
+    <Flex>
+      <Flex direction="row">
+        <Text
+          typography={active ? Typography.Body_1_B : Typography.Body_1_M}
+          color={active ? colors.orange : colors.black40}>
+          {children}
+        </Text>
+        {icon ? (
+          active ? (
+            <>
+              <Spacing width={2} />
+              <StyledIcon source={ic_heart_orange} />
+            </>
+          ) : (
+            <>
+              <Spacing width={2} />
+              <StyledIcon source={ic_heart_black20} />
+            </>
+          )
+        ) : undefined}
+      </Flex>
       {active && <StyledHorizontalLine />}
-    </>
+    </Flex>
   );
 }
 
@@ -63,4 +87,9 @@ const StyledHorizontalLine = styled.View`
   background-color: ${colors.orange};
   height: 2px;
   margin-top: 4px;
+`;
+
+const StyledIcon = styled.Image`
+  width: 24px;
+  height: 24px;
 `;
