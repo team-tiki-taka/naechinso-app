bbf38ada17932e54e24318e1a3f58134dbf64f7e
manyyeon <udayeon@naver.com>
2022-11-06 06:16:35 +0900

    data 없을 때 ui

--- src/screens/my-page/MyPageHomeScreen.tsx
+++ src/screens/my-page/MyPageHomeScreen.tsx
@@ -1,6 +1,7 @@
 import {likedMatchesState, reportFlagState} from '@atoms/matching';
 import {Spacing} from '@components/common';
 import {
+  Flex,
   Screen,
   StyledInnerContainer,
   TransparentGradient,
@@ -8,7 +9,7 @@ import {
 import colors from '@constants/color';
 import {useNavigation} from '@hooks/navigation';
 import {MyPageHeader} from '@screens/my-page/components/my-page-header/MyPageHeader';
-import React from 'react';
+import {default as React, default as React} from 'react';
 import {SectionList} from 'react-native';
 import {useRecoilValue} from 'recoil';
 import styled from 'styled-components/native';
@@ -16,6 +17,13 @@ import {ToggleMenu} from './components/my-page-header';
 import {ProfileHeader} from './components/my-page-header/ProfileHeader';
 import {ProfileCard} from './components/ProfileCard';
 import {useToggleMenu} from './hooks';
+
+import {Text, Typography} from '@components/text';
+
+import img_no_complete_heart from '@assets/images/img_no_complete_heart.png';
+import img_no_give_heart from '@assets/images/img_no_give_heart.png';
+import img_no_take_heart from '@assets/images/img_no_take_heart.png';
+import React, {useEffect, useState} from 'react';
 
 export function MyPageHomeScreen() {
   const navigation = useNavigation();
@@ -54,11 +62,49 @@ export function MyPageHomeScreen() {
     },
   ];
 
+  const [noDataInfo, setNoDataInfo] = useState<{
+    image: string;
+    title: string;
+    content: string;
+  }>({});
+
+  useEffect(() => {
+    if (selectedMenu.menu === '보낸 호감') {
+      setNoDataInfo({
+        image: img_no_give_heart,
+        title: '보낸 호감이 없어',
+        content: '마음에 쏙 드는 사람을 \n찾을 수 있도록 더 노력할게!',
+      });
+    } else if (selectedMenu.menu === '받은 호감') {
+      setNoDataInfo({
+        image: img_no_take_heart,
+        title: '받은 호감이 없어',
+        content: '조금만 기다려봐!',
+      });
+    } else {
+      setNoDataInfo({
+        image: img_no_complete_heart,
+        title: '둘 다 호감이 없어',
+        content: '서로 마음에 쏙 드는 사람이\n곧 나타날거야 :)',
+      });
+    }
+  }, [selectedMenu]);
+
   return (
     <Screen>
       <MyPageHeader />
       <SectionList
         sections={data}
+        renderSectionFooter={item =>
+          item.section.title === 'CardList' &&
+          !item.section.data.length && (
+            <NoData
+              image={noDataInfo?.image}
+              title={noDataInfo?.title}
+              content={noDataInfo?.content}
+            />
+          )
+        }
         renderItem={item => {
           if (item.section.title === 'CardList') {
             return (
@@ -92,3 +138,33 @@ export function MyPageHomeScreen() {
 const InnerContainer = styled(StyledInnerContainer)`
   background-color: ${colors.neural};
 `;
+
+const StyledImage = styled.Image`
+  width: 154px;
+  height: 124px;
+`;
+
+function NoData({
+  image,
+  title,
+  content,
+}: {
+  image: string;
+  title: string;
+  content: string;
+}) {
+  return (
+    <Flex.CenterVertical>
+      <Spacing height={116} />
+      <StyledImage source={image} />
+      <Spacing height={11} />
+      <Text typography={Typography.Subtitle_1_B} color={colors.black20}>
+        {title}
+      </Text>
+      <Spacing height={6} />
+      <Text typography={Typography.Body_2_M} color={colors.black40} center>
+        {content}
+      </Text>
+    </Flex.CenterVertical>
+  );
+}
